5efed71b9d2abddc38c2ba8cd9601d143a262b2d
Dan-Hen <leo.clot16@gmail.com>
2021-03-13 18:49:28 +0100

    Updates

--- main.1e43358e.js
+++ main.1e43358e.js
@@ -15675,7 +15675,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61233" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65111" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
